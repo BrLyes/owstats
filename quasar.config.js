94e9fcb6b34d8cf182bf46f2b2c854609724d45e
lyesb <lyes.b@createam-dz.com>
2022-12-04 17:05:23 +0100

    V0.2.1
    Restored asar packing

--- quasar.config.js
+++ quasar.config.js
@@ -231,7 +231,6 @@ module.exports = configure(function (ctx) {
 
       builder: {
         // https://www.electron.build/configuration/configuration
-        asar: false,
         appId: 'owstats',
         extraFiles: [
           "tracker/**/*",
--- src-electron/electron-main.js
+++ src-electron/electron-main.js
@@ -1,5 +1,5 @@
-import { dialog, app, BrowserWindow, nativeTheme, ipcMain } from 'electron';
-import { autoUpdater } from 'electron-updater';
+import {dialog, app, BrowserWindow, nativeTheme, ipcMain} from 'electron';
+import {autoUpdater} from 'electron-updater';
 import path from 'path';
 import os from 'os';
 
@@ -10,83 +10,104 @@ try {
   if (platform === 'win32' && nativeTheme.shouldUseDarkColors === true) {
     require('fs').unlinkSync(path.join(app.getPath('userData'), 'DevTools Extensions'))
   }
-} catch (_) { }
+} catch (_) {
+}
 
 let mainWindow
 
-function createWindow () {
+function createWindow() {
   /**
    * Initial window options
    */
   mainWindow = new BrowserWindow({
-    icon: path.resolve(__dirname, 'icons/logo.png'), // tray icon
-    autoHideMenuBar:true,
-    width: 1600,
-    height: 720,
-    useContentSize: true,
-    webPreferences: {
-      sandbox: false,
-      contextIsolation: true,
-      preload                : path.resolve(__dirname,
-                                            'electron-preload.js'),
-    }
-  });
+                                   icon           : path.resolve(__dirname, 'icons/logo.png'), // tray icon
+                                   autoHideMenuBar: true,
+                                   width          : 1600,
+                                   height         : 720,
+                                   useContentSize : true,
+                                   webPreferences : {
+                                     sandbox         : false,
+                                     contextIsolation: true,
+                                     preload         : path.resolve(__dirname,
+                                                                    'electron-preload.js'),
+                                   }
+                                 });
 
   mainWindow.loadURL(process.env.APP_URL)
 
-  // if (process.env.DEBUGGING) {
-  //   mainWindow.webContents.openDevTools()
-  // } else {
-  //   mainWindow.webContents.on('devtools-opened', () => {
-  //     mainWindow.webContents.closeDevTools()
-  //   })
-  // }
+  if (process.env.DEBUGGING) {
+    mainWindow.webContents.openDevTools()
+  }
+  else {
+    mainWindow.webContents.on('devtools-opened', () =>
+    {
+      mainWindow.webContents.closeDevTools()
+    })
+  }
 
-  mainWindow.on('close', () => {
+  mainWindow.on('close', () =>
+  {
     mainWindow.webContents.send('close');
   });
 
-  mainWindow.on('closed', () => {
+  mainWindow.on('closed', () =>
+  {
     mainWindow = null
   });
 
   mainWindow.webContents.send('created');
-
-  mainWindow.webContents.openDevTools()
-
 }
 
-app.whenReady().then(() => {
-  autoUpdater.autoDownload=true;
-  autoUpdater.checkForUpdates();
-});
+app.whenReady().then(
+  () =>
+  {
+    createWindow();
+    autoUpdater.autoDownload=false;
+    autoUpdater.checkForUpdates();
+  });
 
-app.on('window-all-closed', () => {
+app.on('window-all-closed', () =>
+{
   if (platform !== 'darwin') {
     app.quit()
   }
 })
 
-app.on('activate', () => {
-  mainWindow.webContents.send("closing");
+app.on('activate', () =>
+{
+  mainWindow.webContents.send('closing');
   if (mainWindow === null) {
     createWindow()
   }
 })
 
-autoUpdater.on("update-available", (info) => {
+autoUpdater.on('update-available', () =>
+{
   const dialogOpts = {
-    title: 'Update available',
+    title  : 'Update available',
+    type: 'info',
+    buttons: ['Download update in the background', 'Ask me on next restart'],
     message: 'An update is being downloaded, please wait. OWStats will restart automatically'
   };
-  dialog.showMessageBox(dialogOpts);
+  dialog.showMessageBox(dialogOpts).then((result) => {
+    if(result.response===0){
+      autoUpdater.downloadUpdate();
+    }
+  });
 });
 
-autoUpdater.on("update-not-available", (info) => {
-  app.whenReady().then(createWindow)
-});
-
-
-autoUpdater.on("update-downloaded", () => {
-  autoUpdater.quitAndInstall();
+autoUpdater.on('update-downloaded', () =>
+{
+  const dialogOpts = {
+    title  : 'Update downloaded',
+    type: 'info',
+    buttons: ['Quit and update', 'later'],
+  };
+  dialog.showMessageBox(dialogOpts).then(
+    (result) =>
+    {
+      if (result.response === 0) {
+        autoUpdater.quitAndInstall();
+      }
+    });
 });
